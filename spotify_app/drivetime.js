dd8b0ad482f498323de5dd07e732b67c8d90f803
Tom Armitage <tom@infovore.org>
2011-12-03 13:08:05 +0000

    Now we support dragndrop playlists.

--- spotify_app/drivetime.js
+++ spotify_app/drivetime.js
@@ -10,6 +10,26 @@ function init() {
       updatePageWithTrackDetails();
     }
   });
+  
+  sp.core.addEventListener("linksChanged", function(event) {
+    var playlistURI = sp.core.getLinks()[0];
+    var playlist = sp.core.getPlaylist(playlistURI);
+    var tracks = new Array();
+    for (var i=0; i < playlist.length; i++) {
+      var track = playlist.getTrack(i);
+      tracks.push(track);
+    };
+    // now we display those
+    var playlistElement = document.getElementById("playlist");
+    playlistElement.innerHTML = "";
+    var tracksHTML = "";
+    for (var i=0; i < tracks.length; i++) {
+      tracksHTML = tracksHTML + "<tr>" + "<td>" + tracks[i].name + "</td>" + "<td>" + tracks[i].album.name + "</td>" + "<td>" + tracks[i].album.artist.name + "</td>" + "<td>" + millisToTimeString(tracks[i].duration) + "</td>" + "</tr>"
+    };
+    
+    playlistElement.innerHTML = "<table><thead><tr><th>Track</th><th>Album</th><th>Artist</th><th>Duration</th></tr></thead><tbody>" + tracksHTML  + "</tbody></table>"
+  });
+  
 }
 
 function updatePageWithTrackDetails() {
@@ -25,4 +45,14 @@ function updatePageWithTrackDetails() {
     var track = playerTrackInfo.track;
     nowPlaying.innerText = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
   }
+}
+
+function millisToTimeString(millis) {
+  var seconds = millis / 1000;
+  var fullMinutes = Math.floor(seconds / 60);
+  var remainingSeconds = seconds - (60 * fullMinutes);
+  if(remainingSeconds.toString().length < 2) {
+    remainingSeconds = "0" + remainingSeconds;
+  }
+  return fullMinutes + ":" + remainingSeconds;
 }
