338864ce312193fc23a46631bbecbc193edc5916
Tom Armitage <tom@infovore.org>
2011-12-03 12:29:54 +0000

    First proof of now-playing.

--- spotify_app/drivetime.js
+++ spotify_app/drivetime.js
@@ -3,5 +3,26 @@ sp = getSpotifyApi(1);
 exports.init = init;
 
 function init() {
-    console.log("init()");
+  updatePageWithTrackDetails();
+  sp.trackPlayer.addEventListener("playerStateChanged", function (event) {
+    // Only update the page if the track changed
+    if (event.data.curtrack == true) {
+      updatePageWithTrackDetails();
+    }
+  });
+}
+
+function updatePageWithTrackDetails() {
+
+  var nowPlaying = document.getElementById("nowplaying");
+
+  // This will be null if nothing is playing.
+  var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack();
+
+  if (playerTrackInfo == null) {
+    nowPlaying.innerText = "Nothing playing!";
+  } else {
+    var track = playerTrackInfo.track;
+    nowPlaying.innerText = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
+  }
 }
