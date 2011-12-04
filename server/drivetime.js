d9f03ea0f2a849a360e38a69e2d4db4ef8d49c35
Tom Taylor <tom@tomtaylor.co.uk>
2011-12-04 13:21:38 +0000

    rename server

--- server/drivetime.js
+++ server/drivetime.js
@@ -8,171 +8,178 @@ app.listen(8081);
 function handler(req, res) {}
 
 var broadcasters = {};
+var clients = [];
 
 io.sockets.on('connection', function (socket) {
+  var client = new Client(socket);
+});
 
-  util.debug('socket connected: ' + socket.id);
-  sendBroadcastersToSocket(socket);
+function Client(socket) {
+  this.socket = socket;
+  this.status = "stopped";
+  this.statusMetadata = {};
 
-  socket.on('broadcasting', function(data) {
-    console.log('socket ('+ socket.id + ') asked to broadcast: ' + util.inspect(data));
+  clients.push(this);
+  this.sendServerTime();
+  this.refreshBroadcasters();
 
-    if (data && data.username && data.track && data.timestamp) {
-      var username = data.username;
-      var track = data.track;
-      var timestamp = data.timestamp;
+  var that = this;
+  this.socket.on('broadcast', function(data) { that.onBroadcast(data); } );
+  this.socket.on('listen', function(data) { that.onListen(data); } );
+  this.socket.on('stop', function(data) { that.onStop(data); });
+  this.socket.on('disconnect', function() { that.onDisconnect(); } );
+}
 
-      var broadcaster = broadcasters[username];
+Client.prototype.onBroadcast = function(data) {
+  this.stopListening();
 
-      if (broadcaster) {
-        // set the new track and timestamp
-        broadcaster['track'] = track;
-        broadcaster['timestamp'] = timestamp;
+  var username = data.username;
+  var track = data.track;
+  var timestamp = data.timestamp;
+  var name = data.name;
 
-      } else {
-        // if we've not seen this broadcaster before, then we setup up a blank array of listeners
-        broadcaster = {
-          'listeners': [],
-          'track': track,
-          'timestamp': timestamp
-        };
+  this.status = "broadcasting";
+  this.statusMetadata = {
+    'broadcastUsername': username
+  };
 
-        broadcasters[username] = broadcaster;
+  var broadcaster = broadcasters[username];
+  util.debug('socket ('+ this.socket.id + ') is broadcasting: ' + track);
 
-      }
+  if (broadcaster) {
+    // set the new track and timestamp
+    broadcaster['track'] = track;
+    broadcaster['timestamp'] = timestamp;
+    broadcaster['name'] = name;
 
-      // tell everyone listening to this broadcaster that the track has changed
-      _.each(broadcaster['listeners'], function(listener) {
-        listener.emit('play', {
-          'track': broadcaster['track'],
-          'timestamp': broadcaster['timestamp']
-        });
-        util.debug('told socket ('+ socket.id + ') to play: ' + util.inspect(broadcaster['track']));
+  } else {
+    // if we've not seen this broadcaster before, then we setup up a blank array of listeners
+    broadcaster = {
+      'listeners': [],
+      'track': track,
+      'timestamp': timestamp,
+      'name': name
+    };
 
-      });
+    broadcasters[username] = broadcaster;
 
-      sendBroadcastersToAll(socket);
-    }
+    refreshAllBroadcasters();
+  }
 
-  });
-
-  socket.on('listen_to', function(data) {
-    util.debug('socket ('+ socket.id + ') asked to listen to ' + util.inspect(data));
-    var username = data.username;
-    var broadcaster = broadcasters[username];
-
-    if (broadcaster) {
-      util.debug("found broadcaster: "+ util.debug(broadcaster));
-    } else {
-      util.debug("broadcaster "+ util.debug(username) + " wasn't found");
-    }
-
-    removeSocketFromAllListeners(socket);
-
-    // if this broadcast exists, and they're not already a listener, then add them
-    if (broadcaster && !_.include(broadcaster['listeners'], socket)) {
-      util.debug('socket ('+socket.id+') added itself to the listeners for: '+ util.inspect(broadcasters[username]));
-      broadcaster['listeners'].push(socket);
-    }
-
-    // if the broadcaster exists, then tell the client to play the track
-    if (broadcaster) {
-      socket.emit('play', {
-        'timestamp': broadcaster.timestamp,
-        'track': broadcaster.track,
-        'username': broadcaster.username
-      });
-      sendBroadcastersToAll(socket);
-    }
+  var that = this;
+  // tell everyone listening to this broadcaster that the track has changed
+  _.each(broadcaster['listeners'], function(listener) {
+    listener.emit('play', {
+      'track': broadcaster['track'],
+      'timestamp': broadcaster['timestamp']
+    });
+    util.debug('told socket ('+ that.socket.id + ') to play: ' + util.inspect(broadcaster['track']));
 
   });
 
-  socket.on('stop_listening', function(data) {
-    var username = data.username;
-    var broadcaster = broadcasters[username];
+};
 
-    if (broadcaster) {
-      util.debug('socket ('+socket.id+') stopped listening to broadcaster: ' + broadcaster.username);
-      broadcaster['listeners'] = _.without(broadcaster['listeners'], socket);
-    }
+Client.prototype.onListen = function(data) {
+  this.stopBroadcasting();
+  this.stopListening();
 
-  });
+  var username = data.username;
+  var broadcaster = broadcasters[username];
 
-  socket.on('stop_broadcasting', function(data) {
-    var username = data.username;
-    var broadcaster = broadcasters[username];
+  this.status = "listening";
+  this.statusMetadata = {
+    'listeningTo': username
+  }
+
+  // if this broadcast exists, and they're not already a listener, then add them
+  if (broadcaster && !_.include(broadcaster['listeners'], this.socket)) {
+    util.debug('socket ('+this.socket.id+') added itself to the listeners for: '+ util.inspect(broadcasters[username]));
+    broadcaster['listeners'].push(this.socket);
+  }
+
+  // if the broadcaster exists, then tell the client to play the track
+  if (broadcaster) {
+    this.socket.emit('play', {
+      'timestamp': broadcaster.timestamp,
+      'track': broadcaster.track,
+    });
+  }
+
+};
+
+Client.prototype.onDisconnect = function() {
+  this.stopBroadcasting();
+  this.stopListening();
+  clients = _.without(clients, this);
+};
+
+Client.prototype.onStop = function(data) {
+  this.stopBroadcasting();
+  this.stopListening();
+  this.status = "stopped";
+  this.statusMetadata = {};
+};
+
+Client.prototype.stopBroadcasting = function() {
+  if (this.status == 'broadcasting') {
+    util.debug("Broadcast metadata: " + util.inspect(this.statusMetadata));
+    var broadcastUsername = this.statusMetadata.broadcastUsername;
+    var broadcaster = broadcasters[broadcastUsername];
+
+    util.debug('stopped ' + broadcastUsername + ' broadcasting');
 
     // if the broadcast exists, tell all the clients to stop listening, and remove the broadcaster
     if (broadcaster) {
       _.each(broadcaster['listeners'], function(listener) {
-        listener.emit('stop_listening', { 'username': username });
+        listener.emit('stop', { 'username': broadcastUsername });
+
       });
 
-      delete broadcasters[username] ;
+      delete broadcasters[broadcastUsername];
     }
 
-    sendBroadcastersToAll(socket);
+    refreshAllBroadcasters();
+  }
+};
 
-  });
+Client.prototype.stopListening = function() {
+  if (this.status == 'listening') {
 
-  socket.on('disconnect', function () {
-    util.debug('socket ('+ socket.id + ') disconnected');
-    // loop through all the broadcasters, removing the socket from the listeners
-    removeSocketFromAllListeners(socket);
-  });
+    var username = this.statusMetadata.listeningTo;
+    var broadcaster = broadcasters[username];
 
+    if (broadcaster && _.include(broadcaster.listeners, this.socket)) {
+      util.debug('stopped listening to ' + username);
+      broadcaster['listeners'] = _.without(broadcaster['listeners'], this.socket);
+      util.debug('removed socket ('+ this.socket.id + ') from broadcaster: ' + broadcaster.username);
+    }
+  }
+};
 
-});
-
-function cleanBroadcasters() {
+Client.prototype.refreshBroadcasters = function() {
   var cleanBroadcasters = [];
 
   _.each(broadcasters, function(broadcaster, username) {
     var cleanBroadcaster = {
       'username': username,
       'track': broadcaster.track,
-      'timestamp': broadcaster.timestamp
+      'timestamp': broadcaster.timestamp,
+      'name': broadcaster.name
     };
 
     cleanBroadcasters.push(cleanBroadcaster);
   });
 
-  return cleanBroadcasters;
+  this.socket.emit('broadcasters', {'broadcasters': cleanBroadcasters});
+};
 
-}
+Client.prototype.sendServerTime = function() {
+  var now = new Date();
+  this.socket.emit('time', { 'time': now.getTime() });
+};
 
-function sendBroadcastersToSocket(socket) {
-  socket.emit('broadcasters', {'broadcasters': cleanBroadcasters()});
-}
-
-function sendBroadcastersToAll(socket) {
-  var cleanBroadcasters = [];
-
-  _.each(broadcasters, function(broadcaster, username) {
-    var cleanBroadcaster = {
-      'username': username,
-      'track': broadcaster.track,
-      'timestamp': broadcaster.timestamp
-    };
-
-    cleanBroadcasters.push(cleanBroadcaster);
+function refreshAllBroadcasters() {
+  _.each(clients, function(client) {
+    client.refreshBroadcasters();
   });
-
-  socket.broadcast.emit('broadcasters', {'broadcasters': cleanBroadcasters});
-  socket.emit('broadcasters', {'broadcasters': cleanBroadcasters});
-
-}
-
-function removeSocketFromAllListeners(socket) {
-  _.each(broadcasters, function(broadcaster) {
-    _.each(broadcaster['listeners'], function(listeners) {
-      if (_.include(listeners, socket)) {
-        broadcaster['listeners'] = _.without(broadcaster['listeners'], socket);
-        util.debug('removed socket ('+ socket.id + ') from broadcaster: ' + broadcaster.username);
-      }
-
-    });
-
-  });
-
 }
